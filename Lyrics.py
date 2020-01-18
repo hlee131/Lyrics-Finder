4988c54642139d03b6e2b8d9315e69594fc70e93
hlee131 <mrhansellee@gmail.com>
2020-01-18 10:20:45 -0800

    Reworked searching functionality

--- Lyrics.py
+++ Lyrics.py
@@ -5,69 +5,49 @@ import sys
 
 def write():
     # Writes to file
-    file = open('%s.txt' % (search.songname), 'w')
-    file.write(search.header)
-    file.write(search.lyrics)
-    file.close()
+    with open('%s.txt' % (search.songname), 'w') as file:
+        file.write(search.header)
+        file.write(search.lyrics)
 
 def search(url=None):
     
-    if url == None:
-        # Asks for name and artist and opens url
-        search.name = input('What is the name of your song? ')
-        name = search.name.replace(' ', '').lower()
-        name = str(name)
-        artist = input('Who is the artist of your song? ').lower()
-        artist = artist.replace(' ', '')
-        yn = False
-        yesno = input("Would you like to print the lyrics to a file? (y/n) ")
-        validchar = ['y', 'n']
-
-        #  Validates answer
-        while not yn:
-            if yesno not in validchar:
-                yesno = input("'%s' is an invalid input. Try again. (y/n) " % (yesno)) 
-
-            elif yesno in validchar:
-                yn = True
-
-        search.url = 'https://azlyrics.com/lyrics/%s/%s.html' % (artist, name)
-
-    else: 
-        search.url = url 
+    search.url = url
 
     search.source = requests.get(search.url).text
 
     soup = BeautifulSoup(search.source, 'lxml')
 
     # Find song name and artist
-    try:
-        songname = soup.find_all('b')[1].text
-        search.songname = songname[1:-1]
-        artist = soup.find('div', class_='col-xs-12 col-lg-8 text-center').find('div', class_='lyricsh').h2.text
-        search.artist = artist[:-7]
-        search.lyrics = soup.find('div', class_=None).text.strip()
-        search.header = '\n%s by %s \n' % (songname, artist)
-        search.valid = True
+    songname = soup.find_all('b')[1].text
+    search.songname = songname[1:-1]
+    artist = soup.find('div', class_='col-xs-12 col-lg-8 text-center').find('div', class_='lyricsh').h2.text
+    search.artist = artist[:-7]
+    search.lyrics = soup.find('div', class_=None).text.strip()
+    search.header = '\n%s by %s \n' % (songname, artist)
+    search.valid = True
 
-        # Calls write only if user said yes
-        if yesno == 'y':
-            write()
+    yesno = input('Would you like to write the lyrics to a file? (y/n) ')
+    while yesno not in ['y', 'n']:
+        yesno = input("Please use either 'y' or 'n' ")
 
-        # Prints lyrics
-        print(search.header)
-        print(search.lyrics +'\n')
+    # Calls write only if user said yes
+    if yesno == 'y':
+        write()
 
-        search.again = input('Would you like to try another song? (y/n) ')
-        while search.again not in validchar:
-            search.again = input("'%s' is an invalid answer. Please try again. (y/n) " % (search.again))
+    # Prints lyrics
+    print(search.header)
+    print(search.lyrics +'\n')
 
-    except:
-        search.valid = False
-        search.again = 'n'
-        search.songname = search.name
-        search.artist = artist
-        search.query = "'%s' by '%s'" % (search.songname, search.artist)
+    search.again = input('Would you like to try another song[1] or quit[2]? ')
+
+    while search.again not in ['1', '2']:
+        search.again = input("'%s' is an invalid answer. Please try again. " % (search.again))
+
+    if search.again == '1':
+        start()
+
+    elif search.again == '2':
+        sys.exit()
 
 def find(searchterm):
     query = searchterm.split()
@@ -75,12 +55,22 @@ def find(searchterm):
     newurl = 'https://search.azlyrics.com/search.php?q=%s' % (queryinlink)
     newsource = requests.get(newurl).text
     newsoup = BeautifulSoup(newsource, 'lxml')
-    topfive = newsoup.find_all('td', class_='text-left visitedlyr', limit = 5)
+    panels = newsoup.find_all('div', class_='panel')
+
+    if len(panels) > 1:
+        ourpanel = panels[len(panels) - 1]
+
+    elif len(panels) == 1:
+        ourpanel = panels[0]
+
+    topfive = ourpanel.find_all('td', class_='text-left visitedlyr', limit = 5)
+    
     keys = ['1', '2', '3', '4', '5']
     values = []
     urls = []
     for song in topfive:
-        values.append('%s by %s' % (song.a.b.text, song.a.b.next_element.next_element.next_element.text))
+        artist = str(song.a.b.next_element.next_element.next_element)
+        values.append('%s by %s' % (song.a.b.text, artist[3:-4]))
         urls.append(song.a['href'])
 
     options = {k:v for k,v in zip(keys, values)}
@@ -88,35 +78,30 @@ def find(searchterm):
     for key, value in options.items():
         print('%s : %s' % (key, value))
 
-    print(urls)
-    selection = input('Please choose one of the options with a number from 1-5. ')
+    if len(options) == 0:
+        answer = input("Sorry, we couldn't find anything for your search term, would you like to try another song[1],\nor quit[2]?")
 
-    while selection not in keys:
-        selection = input('Please choose one of the options with a number from 1-5.')
+        if answer == '1':
+            start()
 
-    search(urls[int(selection) - 1])
+        elif answer == '2':
+            sys.exit()
+    else:
+        selection = input('Please choose one of the options with a number from 1-5. ')
+
+        while selection not in keys:
+            selection = input('Please choose one of the options with a number from 1-5.')
+
+        search(urls[int(selection) - 1])
 
 
-find('this by that')
+def start():
+    search = input('What song would you like to find the lyrics for today? ')
+    find(search)
+
+start()
 
 
-# # Runs search()
-# search()
-# while search.again == 'y':
-#     search()
 
-# while not search.valid:
-#     answers = ['1', '2']
-#     answer = input("Sorry we couldn't find %s. Would you like to try another song [1]\nor quit [2]? " % (search.query))
-#     while answer not in answers:
-#         answer = input("Please use either 1 or 2 to answer. Would you like to try another song [1] or quit [2]? ")
-
-#     if answer == '1':
-#         search()
-
-#     elif answer == '2':
-#         break
-
-# sys.exit()
 
 
